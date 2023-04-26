4570ad6c7966787d18fcef9341e2b5f939dd0208
95rave <ravanaap@code.edu.az>
2023-04-26 04:26:36 +0400

    Student and Group class

---
+++ Group.cs
@@ -0,0 +1,48 @@
+﻿using StudentAndGroupClass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAndGroupClass.Models
+{
+    public class Group
+    {
+        public int No { get; set; }
+        public int Limit { get; set; }
+        public Student[] Students { get; private set; }
+
+        public Group(int no, int limit)
+        {
+            No = no;
+            Limit = limit;
+            Students = new Student[Limit];
+        }
+
+        public void AddStudent(Student student)
+        {
+            if (Students.Count(s => s != null) < Limit)
+            {
+                Student[] newStudents = new Student[Students.Length + 1];
+                for (int i = 0; i < Students.Length; i++)
+                {
+                    newStudents[i] = Students[i];
+                }
+                newStudents[newStudents.Length - 1] = student;
+                Students = newStudents;
+            }
+            else
+            {
+                Console.WriteLine("Grup doludur.");
+            }
+        }
+
+        public Student[] GetStudents()
+        {
+            return Students;
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using StudentAndGroupClass.Models;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Group group = new Group(137,3);
+
+        Student student1 = new Student("eli", "eliyev");
+        group.AddStudent(student1);
+
+        Student student2 = new Student("veli", "veliyev");
+        group.AddStudent(student2);
+
+        Student student3 = new Student("pirveli", "piriyev");
+        group.AddStudent(student3);
+
+        Student student4 = new Student("gultac", "nebilime");
+        group.AddStudent(student4);
+
+        Student[] students = group.GetStudents();
+        foreach (Student student in students)
+        {
+            if (student != null)
+            {
+                Console.WriteLine("Name: {0} {1}", student.Name, student.Surname);
+            }
+        }
+
+    }
+}
